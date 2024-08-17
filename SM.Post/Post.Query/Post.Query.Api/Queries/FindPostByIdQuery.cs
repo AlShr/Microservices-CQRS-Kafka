33dacf52d69e2aded5eb75f0b0b825b58300206b
AlShr <shramko.zp@gmail.com>
2024-08-17 15:51:22 +0300

    Queries, Query Dispatching and Handling

---
+++ SM.Post/Post.Query/Post.Query.Api/Queries/FindPostByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using CQRS.Core.Queries;
+
+namespace Post.Query.Api.Queries
+{
+  public class FindPostByIdQuery : BaseQuery
+  {
+    public Guid Id { get; set; }
+
+  }
+}
---
+++ SM.Post/Post.Query/Post.Query.Api/Queries/QueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Post.Query.Domain.Entities;
+using Post.Query.Domain.Repositories;
+
+namespace Post.Query.Api.Queries
+{
+  public class QueryHandler : IQueryHandler
+  {
+    private readonly IPostRepository postRepository;
+
+    public QueryHandler(IPostRepository postRepository)
+    {
+      this.postRepository = postRepository;
+    }
+
+    public async Task<List<PostEntity>> HandlerAsync(FindAllPostsQuery query)
+    {
+      return await postRepository.ListAllAsync();
+    }
+
+    public async Task<List<PostEntity>> HandlerAsync(FindPostByIdQuery query)
+    {
+      var post = await postRepository.GetByIdAsync(query.Id);
+      return new List<PostEntity> { post };
+    }
+
+    public async Task<List<PostEntity>> HandlerAsync(FindPostsByAuthorQuery query)
+    {
+      return await postRepository.ListByAuthorAsync(query.Author);
+    }
+
+    public async Task<List<PostEntity>> HandlerAsync(FindPostsWithCommentsQuery query)
+    {
+      return await postRepository.ListWithCommentsAsync();
+    }
+
+    public async Task<List<PostEntity>> HandlerAsync(FindPostsWithLikesQuery query)
+    {
+      return await postRepository.ListWithLikesAsync(query.NumberOfLikes);
+    }
+  }
+}
